2754c24e9eeaa2dad10eb3c12fe742bded222a0f
GIRISH SAMBHAJI PAWAR <girishpawar172004@gmail.com>
2025-11-17 08:44:07 +0530

    lecture1 completed

--- Python/code.py
+++ Python/code.py
@@ -1,2 +1,29 @@
-print("hello world")
-print("hello world with python")
+# print("hello world")
+# # variables
+# name="girish"
+# age=21
+# PI=3.14
+# isPrime=True
+# print(name,age,PI)
+# identifiers are case sensitive
+#data types
+# integer,string,float,boolean,None
+# print(type(name))
+# print(type(age))
+# print(type(PI))
+# print(type(isPrime))
+# a=5
+# b=10
+# sum=a+b
+# print(sum)
+#operators 
+'''Arithmetic,relational,assignment,logical'''
+# type conversion-implicit
+# type casting-explicit     
+# user input
+# a=input("enter value of a ")
+# print(a)
+num_1=int(input("enter number 1: "))
+num_2=int(input("enter number 2: "))
+avrg=(num_1+num_2)/2
+print("AVERAGE : ",avrg)
